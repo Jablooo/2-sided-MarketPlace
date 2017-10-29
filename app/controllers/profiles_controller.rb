1e0d31d8df75277e4c97809593c323d564eb3a39
Jon Ablondi <jonnyablondi@hotmail.com>
2017-10-29 16:37:42 +1100

    edited form for new profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -25,6 +25,7 @@ class ProfilesController < ApplicationController
   # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
+    @profile.user = current_user
 
     respond_to do |format|
       if @profile.save
@@ -69,6 +70,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:user_id, :avatar_data, :first_name, :last_name, :dob, :postcode, :sport_play, :sport_coach)
+      params.require(:profile).permit(:user_id, :avatar, :first_name, :last_name, :dob, :postcode, :sport_play, :sport_coach)
     end
 end
